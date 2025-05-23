0241ed43bd754ca8137706d329feed7c7b9ba7c7
Supuni1868 <supuni.20231868@iit.ac.lk>
2025-05-23 10:57:45 +0530

    created some files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import Header from './Components/header/header'
+import Nav from './Components/nav/nav'
+import About from './Components/about/about'
+import Experience from './Components/experience/experience'
+import Services from './Components/services/services'
+import Portfolio from './Components/portfolio/portfolio'
+import Testimonials from './Components/testimonials/testimonials'
+import Contact from './Components/contact/contact'
+import Footer from './Components/footer/footer'
 
 const App = () => {
   return (
-    <div>
-     App 
-    </div>
+    <>
+        <Header/>
+        <Nav/>
+        <About/>
+        <Experience/>
+        <Services/>
+        <Portfolio/>
+        <Testimonials/>
+        <Contact/>
+        <Footer/>
+    </>
   )
 }
 
