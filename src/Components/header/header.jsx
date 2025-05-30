288bc13d6cb5fa5b902fa04bcc930ab89ada4477
Supuni1868 <supuni.20231868@iit.ac.lk>
2025-05-30 14:18:23 +0530

    Add image.

--- src/Components/header/header.jsx
+++ src/Components/header/header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import ME from '../../assets/me.jpeg'
 
 const header = () => {
   return (
@@ -11,9 +12,12 @@ const header = () => {
         <h5 className = "text-light">Software Enginner</h5>
         <CTA />
 
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
 
       
-
+      </div>
     </header>
   )
 }
