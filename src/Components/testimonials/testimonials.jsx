0a5fed204b3e39c38516a15e9464076af8419d45
Supuni1868 <supuni.20231868@iit.ac.lk>
2025-06-01 19:47:58 +0530

    Updated the testimonials.css file

--- src/Components/testimonials/testimonials.jsx
+++ src/Components/testimonials/testimonials.jsx
@@ -8,17 +8,14 @@ const testimonials = () => {
 
       <div className='container testimonials_container'>
         <article className='testimonials'>
-          <div className='client_avatar'>
             <h4 className = 'client_name'>University Of Westminster</h4>
             <small className = 'client_review'>
               BSc.(Hons)(UG) in Computer science in Informatics Institute of Technology.<br/>
               (2023-present)
             </small>
-          </div>
         </article>
 
         <article className='testimonials'>
-          <div className='client_avatar'>
             <h4 className = 'client_name'>G.C.E. Advanced LEVEL Examination</h4>
             <small className = 'client_review'>
              Combined Mathematics - B<br/>
@@ -27,18 +24,15 @@ const testimonials = () => {
              Sujatha Vidyalaya - Matara<br/> 
              2022(2023)
             </small>
-          </div>
         </article>
-        
+
         <article className='testimonials'>
-          <div className='client_avatar'>
             <h4 className = 'client_name'>G.C.E. Ordinary Level Examination</h4>
             <small className = 'client_review'>
               9A Passes<br/>
               Sujatha Vidyalaya - Matara<br/>
               (2018)
             </small>
-          </div>
         </article>
       </div>
       </section>
