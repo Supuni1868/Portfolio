84ba2139b0e2bba36fa17d86f744ab5d119d2c67
Supuni Shashikala <supuni.20231868@iit.ac.lk>
2025-06-07 14:50:41 +0530

    Updated the portfolio.jsx file.

--- src/Components/portfolio/portfolio.jsx
+++ src/Components/portfolio/portfolio.jsx
@@ -1,65 +1,81 @@
-
-
 import React from 'react';
 import './portfolio.css';
 import IMG1 from '../../assets/Portfolio1.jpg';
-import IMG2 from '../../assets/Portfolio2.jpg';
+import IMG2 from '../../assets/Portfolio2.avif';
 import IMG3 from '../../assets/Portfolio3.jpg';
 import IMG4 from '../../assets/Portfolio4.webp';
 import IMG5 from '../../assets/Portfolio5.jpg';
 
+//DO NOT USE IMAGE IN PRODUCTION
 
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'This is a portfolio item title.',
+    github: 'https://github.com'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'This is a portfolio item title.',
+    github: 'https://github.com'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'This is a portfolio item title.',
+    github: 'https://github.com'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'This is a portfolio item title.',
+    github: 'https://github.com'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'This is a portfolio item title.',
+    github: 'https://github.com'
+  },
+  {
+    id: 6,
+    image: IMG1,
+    title: 'This is a portfolio item title.',
+    github: 'https://github.com'
+  }
+]
 
-const portfolio = () => {
+const Portfolio = () => {
   return (
     <section id='portfolio'>
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
       <div className='container portfolio__container'>
-          <article className='portfolio__item'>
-            <div className='portfolio__item-image'>
-                <img src={IMG1} alt="" />
-            </div>
-            <h3>This is a portfolio item title.</h3>
-            <a href = "https://github.com" className='btn' target="_blank">GitHub</a>
-          </article>
+        {
+          data.map(({id, image,title, github, demo}) => {
+            return(
+              <article key={id} className='portfolio__item'>
+                <div className='portfolio__item-image'>
+                  <img src={image} alt={title}/>
+                </div>
 
-          <article className='portfolio__item'>
-            <div className='portfolio__item-image'>
-                <img src={IMG2} alt="" />
-            </div>
-            <h3>This is a portfolio item title.</h3>
-            <a href = "https://github.com" className='btn' target="_blank">GitHub</a>
-          </article>
+                <h3>{title}</h3>
+                <div className='portfolio__item-cta'>
+                  <a href={github} className='btn' target='_blank'>GitHub</a>
+                 
+                </div>
+              </article>
+            )
+          })
+        }
+       
 
-          <article className='portfolio__item'>
-            <div className='portfolio__item-image'>
-                <img src={IMG3} alt="" />
-            </div>
-            <h3>This is a portfolio item title.</h3>
-            <a href = "https://github.com" className='btn' target="_blank">GitHub</a>
-          </article>
-
-          <article className='portfolio__item'>
-            <div className='portfolio__item-image'>
-                <img src={IMG4} alt="" />
-            </div>
-            <h3>This is a portfolio item title.</h3>
-            <a href = "https://github.com" className='btn' target="_blank">GitHub</a>
-          </article>
-
-          <article className='portfolio__item'>
-            <div className='portfolio__item-image'>
-                <img src={IMG1} alt="" />
-            </div>
-            <h3>This is a portfolio item title.</h3>
-            <a href = "https://github.com" className='btn' target="_blank">GitHub</a>
-          </article>
-      </div>
-      
+     </div>
     </section>
-  )
-}
+  );
+};
 
-export default portfolio
+export default Portfolio;
