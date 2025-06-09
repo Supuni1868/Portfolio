2040181c62c72900c7cbce7d2a9fdd9c216f6c72
Supuni Shashikala <supuni.20231868@iit.ac.lk>
2025-06-09 09:20:59 +0530

    Update images

--- src/Components/about/about.jsx
+++ src/Components/about/about.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './about.css'
-import me from '../../assets/me.jpg'
+import me from '../../assets/Me1.png'
 import { FaAward } from 'react-icons/fa'
 import {FiUsers} from 'react-icons/fi'
 import {VscFolderLibrary} from 'react-icons/vsc'
--- src/Components/header/header.jsx
+++ src/Components/header/header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/Me.png'
+import ME from '../../assets/me.png'
 import HeaderSocials from  './HeaderSocials'
 import {motion} from 'framer-motion'
 import { fadeIn } from '../../variants'
