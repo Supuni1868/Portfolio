e0821ea8d0478d9b45653412b292038660a079dc
Supuni Shashikala <supuni.20231868@iit.ac.lk>
2025-06-10 07:44:20 +0530

    remane pictures

--- src/Components/header/header.jsx
+++ src/Components/header/header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/me.png'
+import ME from '../../assets/ME.png'
 import HeaderSocials from  './HeaderSocials'
 import {motion} from 'framer-motion'
 import { fadeIn } from '../../variants'
